27ecb81015266fe356b0a84d9aa087e75ebe8048
Srikant Patnaik <srikant@fossee.in>
2013-04-15 16:29:01 +0530

    fixed the mysql database error by removing the old tables, syncdb doesn't rename the existing table

--- app_add_database/views.py
+++ app_add_database/views.py
@@ -1,20 +1,21 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from app_add_database.models import model_to_form
+from models import model_to_form
 
 def app_add_database(request):
-    form = model_to_form()
+    form_db = model_to_form()
     if request.method == 'POST':
-        form = Output(request.POST)
-        if form.is_valid():
-            clean_form = form.cleaned_data
-            value1 = clean_form['input1']
-            value2 = clean_form['input2']
+        form_db = model_to_form(request.POST)
+        if form_db.is_valid():
+            clean_form = form_db.cleaned_data
+            value1 = clean_form['field1']
+            value2 = clean_form['field2']
             out = value1 + value2
-            return render_to_response('add.html', {'calculator':form, 'answer':out}, context_instance = RequestContext(request))
+            form_db.save()
+            return render_to_response('add_db.html', {'calculator':form_db, 'answer':out}, context_instance = RequestContext(request))
         else:
-			return render_to_response('add.html', {'calculator':form}, context_instance = RequestContext(request))
+			return render_to_response('add_db.html', {'calculator':form_db}, context_instance = RequestContext(request))
     else:
-	    return render_to_response('add.html', {'calculator':form}, context_instance = RequestContext(request))
+	    return render_to_response('add_db.html', {'calculator':form_db}, context_instance = RequestContext(request))
 
